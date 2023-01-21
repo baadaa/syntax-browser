d7409b838813d311b180299e5e81311876060895
Bumhan Yu <bumhan.yu@gmail.com>
2023-01-21 01:57:52 -0500

    wip

---
+++ src/utils/utils.ts
@@ -0,0 +1,6 @@
+export const setLocalStorage = (key = '', data = ''): void => {
+  window.localStorage.setItem(key, data);
+};
+
+export const localStorageIsAvailable = (data: string): boolean =>
+  !!window.localStorage.getItem(data);
